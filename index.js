a16d9f89be9b12fe33e0e97c336e5375ca8ae0d6
Khalil Merine <merinekh@gmail.com>
2023-01-19 19:00:38 -0500

    Branch naming

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-const express = require("express"); //Line 1
-const app = express(); //Line 2
-const port = process.env.PORT || 8080; //Line 3
+const data = require("./data/videos.json");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+const { v4: uuidv4 } = require("uuid");
+var cors = require("cors");
 
+app.use(cors());
+app.use(express.json());
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
-// create a GET route
-app.get("/api", (req, res) => {
+// =========================create a GET route for videos=========================================
+app.get("/videos", (req, res) => {
+  res.send(data);
+});
+
+// ==================create a GET route for videos details========================================
+const videosId = require("./data/video-details.json");
+
+app.get("/videos/:id", (req, res) => {
   //Line 9
-  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" }); //Line 10
-}); //Line 11
+  const videoId = videosId.find((element) => element.id === req.params.id);
+  if (videoId) {
+    res.send(videoId);
+  } else {
+    res.send("Couldn't import data from the API");
+  }
+});
+
+// ==================create a POST request for new Video==========================================
+
+app.post("/videos", (req, res) => {
+  const { title, channel, image } = req.body;
+  const newVideo = {
+    id: uuidv4(),
+    title,
+    channel,
+    image,
+  };
+  data.push(newVideo);
+  res.json(newVideo);
+});
