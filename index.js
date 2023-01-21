3b2b6f7ae01ef1dd1597ccbe738fe12452e7865d
Khalil Merine <merinekh@gmail.com>
2023-01-21 16:25:30 -0500

    Add the server routing

--- index.js
+++ index.js
@@ -1,43 +1,14 @@
-const data = require("./data/videos.json");
+const videoRoutes = require("./routes/videos");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
-const { v4: uuidv4 } = require("uuid");
 var cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 // This displays message that the server running and listening to specified port
+
+app.use(express.static("public"));
+
+app.use("/videos", videoRoutes);
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
-
-// =========================create a GET route for videos=========================================
-app.get("/videos", (req, res) => {
-  res.send(data);
-});
-
-// ==================create a GET route for videos details========================================
-const videosId = require("./data/video-details.json");
-
-app.get("/videos/:id", (req, res) => {
-  //Line 9
-  const videoId = videosId.find((element) => element.id === req.params.id);
-  if (videoId) {
-    res.send(videoId);
-  } else {
-    res.send("Couldn't import data from the API");
-  }
-});
-
-// ==================create a POST request for new Video==========================================
-
-app.post("/videos", (req, res) => {
-  const { title, channel, image } = req.body;
-  const newVideo = {
-    id: uuidv4(),
-    title,
-    channel,
-    image,
-  };
-  data.push(newVideo);
-  res.json(newVideo);
-});
