0b8f7a5edd1f92f28e731c74cdc9f40427cbfc2e
Khalil Merine <merinekh@gmail.com>
2023-01-21 17:08:20 -0500

    Add public as Static Ressource

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ var cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
+
+// NEW CODE
+app.use(express.static("public"));
+// END OF NEW CODE
+
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
